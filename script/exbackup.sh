173c374c4ddf5a174f4ac130fe51feff7c37df94
mengshi <meng1985626@163.com>
2016-04-05 11:40:39 +0800

    update script

--- script/exbackup.sh
+++ script/exbackup.sh
@@ -3,9 +3,11 @@
 PORT=$1
 SERVER=$2
 CMD=$3
+HOST=$4
+
 
 ssh -tt -p ${PORT} ${SERVER} <<EOF 
-#        nohup sudo sh /data/backup/xbackup.sh full_backup bkuser bkuser 3306 /data/backup 192.168.2.15 /tmp/mysql.sock /etc/my.cnf &
+#        nohup sudo sh /data/backup/xbackup.sh full_backup bkuser bkuser 3306 /data/backup ${PORT} /tmp/mysql.sock /etc/my.cnf &
 	${CMD}
 	exit 0
 EOF
